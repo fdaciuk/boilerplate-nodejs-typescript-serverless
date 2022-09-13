3ffe36bba3380ae0538161b67048faf4a6f85fd4
Fernando Daciuk <f.daciuk@gmail.com>
2022-09-13 20:11:34 -0300

    feat: add domain configuration plugin and dotenv configuration plugin

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 
 - In `serverless.yml`, update `service` field with the name of your project;
 - In `package.json`, update `name` field with the name of your project;
+
+### Local server
+
+WIP
+
+### Deploy
+
+WIP
+
+### Custom domain
+
+WIP
--- src/functions/fastify-server.ts
+++ src/functions/fastify-server.ts
@@ -2,6 +2,8 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/', (_request, reply) => reply.send({ hello: 'world' }))
+const test = 'TEST'
+const envVar = process.env[test]
+app.get('/', (_request, reply) => reply.send({ hello: 'world', env: envVar ?? 'NADA' }))
 
 export { app }
