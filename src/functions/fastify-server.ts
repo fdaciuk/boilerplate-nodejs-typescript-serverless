b0037e5a1b8a8c2b096588b43a976ba9d0ef1333
Fernando Daciuk <f.daciuk@gmail.com>
2022-09-22 19:18:49 -0300

    feat: prisma and db with docker configuration

--- src/functions/fastify-server.ts
+++ src/functions/fastify-server.ts
@@ -2,8 +2,6 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-const test = 'TEST'
-const envVar = process.env[test]
-app.get('/', (_request, reply) => reply.send({ hello: 'world', env: envVar ?? 'NADA' }))
+app.get('/', (_req, reply) => reply.send({ hello: 'world' }))
 
 export { app }
