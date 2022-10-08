6e0289cda4d6eb8c81988d572dc79c053d1972db
Fernando Daciuk <f.daciuk@gmail.com>
2022-10-08 10:44:59 -0300

    feat: configuration to use with mongodb

--- src/functions/fastify-server.ts
+++ src/functions/fastify-server.ts
@@ -1,7 +1,21 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = fastify()
 
-app.get('/', (_req, reply) => reply.send({ hello: 'world' }))
+app.get('/', async (_req, reply) => {
+  const databaseUrlString = 'DATABASE_URL'
+  console.log('process.env.DATABASE_URL', process.env[databaseUrlString])
+  const newUser = await prisma.user.create({
+    data: {
+      email: 'user@email.com',
+      name: 'The user',
+    },
+  })
+  console.log('\n\n\n', newUser, '\n\n\n')
+  reply.send({ hello: 'world' })
+})
 
 export { app }
